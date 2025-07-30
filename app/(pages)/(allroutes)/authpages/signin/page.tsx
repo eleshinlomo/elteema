cf39a344f92c92f503066aef0e378569712d9d44
eleshinlomo <seun.olatunji2@gmsil.com>
2025-07-30 07:16:31 -0400

    fix

--- app/(pages)/(allroutes)/authpages/signin/page.tsx
+++ app/(pages)/(allroutes)/authpages/signin/page.tsx
@@ -119,8 +119,8 @@ const SigninPage = () => {
                     onClick={handleLogin}
                     className="w-full flex justify-center py-3 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-green-600 hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500 transition-colors duration-200"
                   >
-                    Get Magic Link
-                    {isLoading ? <LoadingState /> : null}
+                    {!isLoading ? 'Get Magic Link' : null}
+                    {isLoading ? 'Sending code...' : null}
                   </button>
                 </form>
 
