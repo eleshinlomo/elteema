0da5e0765c7727fa06238137b9251f312a08cc19
eleshinlomo <seun.olatunji2@gmsil.com>
2025-08-02 06:02:16 -0400

    fix auth

--- app/(pages)/(allroutes)/dashboard/layout.tsx
+++ app/(pages)/(allroutes)/dashboard/layout.tsx
@@ -9,51 +9,44 @@ import LoadingState from "../../../../components/LoadingState";
 import { getLocalUser } from "../../../../components/utils";
 
 interface DashboardProps {
-    children: React.ReactNode
+    children: React.ReactNode;
 }
 
-
-
-const DashboardLayout = ({children}: DashboardProps) => {
-    const { isLoggedIn, user, setUserOrders, isLoading, setIsLoading } = useContext(GeneralContext);
+const DashboardLayout = ({ children }: DashboardProps) => {
+    const { isLoggedIn, user, isLoading } = useContext(GeneralContext);
     const router = useRouter();
     const [authChecked, setAuthChecked] = useState(false);
 
-    
-  
-   useEffect(()=>{
+    useEffect(() => {
+        const checkAuth = () => {
+            const localUser = getLocalUser();
+            if (!localUser || !localUser?.isLoggedIn) {
+                router.push('/authpages/notloggedinpage');
+            } else {
+                setAuthChecked(true);
+            }
+        };
 
-     if (isLoading) {
-        <LoadingState />;
+        if (!isLoading) {
+            checkAuth();
+        }
+    }, [isLoading]);
+
+    if (isLoading || !authChecked) {
+        return <LoadingState />;
     }
 
-
-    const localUser = getLocalUser()
-    if(!isLoggedIn){
-        router.push('/authpages/notloggedinpage')
-    }
-    setAuthChecked(true)
-    },[isLoading, isLoggedIn, authChecked])
-
-
-   
-   
-   
     return (
         <div className="min-h-screen bg-gray-50 pb-24 pt-16">
-            {authChecked && user && <div className="md:flex">
+            <div className="md:flex">
                 <div className="hidden md:block md:w-1/4 md:fixed md:h-screen md:overflow-y-auto">
                     <DashSideBar user={user} userOrders={user?.orders || []} />
                 </div>
                 <div className="w-full md:ml-[25%]">
                     <DashNavBar user={user} userOrders={user?.orders || []} />
-                    <main className="">
-                        {children}
-                    </main>
+                    <main>{children}</main>
                 </div>
             </div>
-           
-             }
         </div>
     );
 };
